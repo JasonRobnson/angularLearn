4fb3b722526192bf7eb20d930d3bcb9eadc1f53c
Robinson <jasonrobinson.dev@gmail.com>
2020-01-16 13:21:10 -0600

    completed toggleCompleted

--- angular-crash-todolist/src/app/services/todo.service.ts
+++ angular-crash-todolist/src/app/services/todo.service.ts
@@ -4,6 +4,11 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Todo } from "../models/Todo";
 import { Observable } from "rxjs";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
 @Injectable({
   providedIn: "root"
 })
@@ -11,8 +16,13 @@ export class TodoService {
   todosUrl: string = "https://jsonplaceholder.typicode.com/todos";
   todosLimit = "?_limit=5";
   constructor(private http: HttpClient) {}
-
+  //Get Todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+  //Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
